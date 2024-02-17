5869671dec594ecc5a552a5062bcaddc01458ee1
Yasser-Aboibrahim <yasser.fayez.aboibrahim@gmail.com>
2024-02-17 11:00:46 +0200

    add icons and colors

--- Hike/Hike/ContentView.swift
+++ Hike/Hike/ContentView.swift
@@ -10,12 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("image-1")
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
     }
 }
 

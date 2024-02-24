366a721f16ef7cab8d11a47d353ae7abb9d53a0b
Yasser-Aboibrahim <yasser.fayez.aboibrahim@gmail.com>
2024-02-24 12:24:21 +0200

    Settings View and add more icons

--- Hike/Hike/Component/CardView.swift
+++ Hike/Hike/Component/CardView.swift
@@ -12,7 +12,8 @@ struct CardView: View {
     
     @State private var imageNumber: Int = 1
     @State private var randomNumber: Int = 1
-    
+    @State private var isShowingSheet : Bool = false
+
     // MARK: - FUNCTIONS
     
     func randomImage() {
@@ -46,10 +47,16 @@ struct CardView: View {
                         
                         Spacer()
                         Button {
-                            //: ACTION
+                            // ACTION: show a sheet
                             print("Button Pressed")
+                            isShowingSheet.toggle()
                         } label: {
                             CustomButtonView()
+                        }
+                        .sheet(isPresented: $isShowingSheet) {
+                            SettingsView()
+                                .presentationDragIndicator(.visible)
+                                .presentationDetents([.medium, .large])
                         }
                     }
                     Text("Fun and enjoyable outdoor activity for friends and families.")
---
+++ Hike/Hike/Component/CustomListRowView.swift
@@ -0,0 +1,62 @@
+//
+//  CustomListRowView.swift
+//  Hike
+//
+//  Created by Yasser Aboibrahim on 24/02/2024.
+//
+
+import SwiftUI
+
+struct CustomListRowView: View {
+    // MARK: - PROPERTIES
+    
+    @State var rowLabel: String
+    @State var rowIcon: String
+    @State var rowContent: String? = nil
+    @State var rowTintColor: Color
+    @State var rowLinkLabel: String? = nil
+    @State var rowLinkDestination: String? = nil
+
+    var body: some View {
+        LabeledContent {
+            // Content
+            if rowContent != nil {
+              Text(rowContent!)
+                .foregroundColor(.primary)
+                .fontWeight(.heavy)
+            } else if (rowLinkLabel != nil && rowLinkDestination != nil) {
+              Link(rowLinkLabel!, destination: URL(string: rowLinkDestination!)!)
+                .foregroundColor(.pink)
+                .fontWeight(.heavy)
+            } else {
+              EmptyView()
+            }
+        } label: {
+            // Label
+            HStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(rowTintColor)
+                    Image(systemName: rowIcon)
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                }
+                Text(rowLabel)
+            }
+        }
+    }
+}
+
+#Preview {
+    List {
+      CustomListRowView(
+        rowLabel: "Website",
+        rowIcon: "globe",
+        rowContent: nil,
+        rowTintColor: .pink,
+        rowLinkLabel: "Credo Academy",
+        rowLinkDestination: "https://credo.academy"
+      )
+    }
+}
